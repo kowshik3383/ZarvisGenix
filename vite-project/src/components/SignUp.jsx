0534ed7fba79f41fc6f7b8c410e4d424059f0335
kowshik <kowshikvalpireddy@gmail.com>
2024-09-15 18:20:33 +0530

    fghdfhgfg

--- vite-project/src/components/SignUp.jsx
+++ vite-project/src/components/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import logo from "../../public/Logo.svg";
+import Logo from "../../public/Logo.svg";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
